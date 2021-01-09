d76ae6ca06c09c43112bed38f5aa42602b92c86a
Collin <52185661+scs727c@users.noreply.github.com>
2021-01-09 17:25:24 -0600

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import {
+    createWebHistory,
+    createRouter
+} from "vue-router";
+import Home from "./components/Home.vue";
+const history = createWebHistory();
+const routes = [
+    {
+        path: "/",
+        component: Home
+    }
+];
+
+const router = createRouter({
+    history,
+    routes
+});
+
+export default router;
