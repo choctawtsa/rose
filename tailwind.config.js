db4dc887d1d0750869a8ff0b6dd9290f1f55b1ee
Collin <52185661+scs727c@users.noreply.github.com>
2021-02-18 23:25:59 -0600

    Further setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.warmGray,
+        orange: colors.orange
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Poppins', 'Inter', ...defaultTheme.fontFamily.sans]
+      },
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
+  ],
 }
