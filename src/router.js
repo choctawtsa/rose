dc52f5043f03a027db3517dcef8e807e533484d3
Collin <52185661+scs727c@users.noreply.github.com>
2021-03-16 00:03:55 -0500

    About page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
 import Company from "./components/Company.vue";
+import About from "./components/About.vue";
 const history = createWebHistory();
 const routes = [
   {
@@ -12,19 +13,23 @@ const routes = [
     path: "/company",
     component: Company,
   },
+  {
+    path: "/about",
+    component: About,
+  },
 ];
 
 const router = createRouter({
   history,
   routes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
     if (to.hash) {
       return { el: to.hash, behavior: "smooth" };
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
     }
-    return { x: 0, y: 0 };
   },
 });
 
